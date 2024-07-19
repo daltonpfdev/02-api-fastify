daa25cbc64b8614cee7ae9cd13f15b81889ba003
daltonpfdev <daltonpf.dev@gmail.com>
2024-07-19 18:06:54 -0300

    Configuração para produção

--- src/env/index.ts
+++ src/env/index.ts
@@ -13,8 +13,9 @@ if (process.env.NODE_ENV === 'test') {
 // Variáveis de Ambiente para ser verificadas com seus devidos tipos
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 // Constante para verificação das variáveis de ambiente
